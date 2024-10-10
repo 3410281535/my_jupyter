ef6fc62341fd09fa2a20db67daeb1df834781876
WangChenSheng <3410281535@qq.com>
2024-10-10 13:50:33 +0800

    daily submit

---
+++ 课程/研究与开发实践/5.坚持100秒/坚持100秒.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+width = 400
+height = 600
+
+class Bullet
+    def __init__(self):
+
+
+pygame.init()
+screen = pygame.display.set_mode(width, height)
+pygame.display.set_caption("坚持100秒")
+img = pygame.image.load()
+
